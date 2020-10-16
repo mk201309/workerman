740248ac6cc326b97c5947c788695401072780a6
beo <beo0523@cchntek.com>
2020-10-16 15:30:04 +0800

    feat(websocket): add websocket test

---
+++ Applications/YourApp/Events.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * This file is part of workerman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link http://www.workerman.net/
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+/**
+ * 用于检测业务代码死循环或者长时间阻塞等问题
+ * 如果发现业务卡死，可以将下面declare打开（去掉//注释），并执行php start.php reload
+ * 然后观察一段时间workerman.log看是否有process_timeout异常
+ */
+//declare(ticks=1);
+
+use \GatewayWorker\Lib\Gateway;
+use \Workerman\Lib\Timer;
+
+/**
+ * 主逻辑
+ * 主要是处理 onConnect onMessage onClose 三个方法
+ * onConnect 和 onClose 如果不需要可以不用实现并删除
+ */
+class Events
+{
+    // 进程启动时设置个定时器。Events中支持onWorkerStart需要Gateway版本>=2.0.4
+    // public static function onWorkerStart()
+    // {
+    //     Timer::add(10, function(){
+    //         echo "timer\n";
+    //     });
+    // }
+
+    /**
+     * 当客户端连接时触发
+     * 如果业务不需此回调可以删除onConnect
+     * 
+     * @param int $client_id 连接id
+     */
+    public static function onConnect($client_id)
+    {
+        // 向当前client_id发送数据 
+        Gateway::sendToClient($client_id, "Hello $client_id\r\n");
+        // 向所有人发送
+        Gateway::sendToAll("$client_id login\r\n");
+
+        // $_SESSION['auth_timer_id'] = Timer::add(30, function($client_id){
+        //     echo "Close Connect!! \n";
+        //     Gateway::closeClient($client_id);
+        // }, array($client_id), false);
+
+    }
+    
+   /**
+    * 当客户端发来消息时触发
+    * @param int $client_id 连接id
+    * @param mixed $message 具体消息
+    */
+   public static function onMessage($client_id, $message)
+   {
+        // Timer::del($_SESSION['auth_timer_id']);
+        // echo "Delete Close Connect Timer!! \n";
+
+        Gateway::sendToClient($client_id, $_SERVER['REMOTE_ADDR']."\r\n");
+
+        // 向所有人发送 
+        Gateway::sendToAll("$client_id said $message\r\n");
+
+
+   }
+   
+   /**
+    * 当用户断开连接时触发
+    * @param int $client_id 连接id
+    */
+   public static function onClose($client_id)
+   {
+       // 向所有人发送 
+       GateWay::sendToAll("$client_id logout\r\n");
+   }
+}
